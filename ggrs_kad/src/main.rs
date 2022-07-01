fa58fac6b84624f05a2a69a14cc191a362704c40
parnikasaxena <paxsethorld@gmail.com>
2022-07-01 15:59:00 -0500

    ggrs

---
+++ ggrs_kad/src/main.rs
@@ -0,0 +1,129 @@
+use std::net::SocketAddr;
+
+use bevy::prelude::*;
+use bevy_ggrs::{GGRSPlugin, SessionType};
+use ggrs::{P2PSession, PlayerType, SessionBuilder, UdpNonBlockingSocket};
+use structopt::StructOpt;
+
+mod box_game;
+use crate::box_game::*;
+
+const FPS: usize = 60;
+const ROLLBACK_DEFAULT: &str = "rollback_default";
+
+// structopt will read command line parameters for u
+#[derive(StructOpt)]
+struct Opt {
+    #[structopt(short, long)]
+    local_port: u16,
+    #[structopt(short, long)]
+    players: Vec<String>,
+    #[structopt(short, long)]
+    spectators: Vec<SocketAddr>,
+}
+
+struct NetworkStatsTimer(Timer);
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // read cmd line arguments
+    let opt = Opt::from_args();
+    let num_players = opt.players.len();
+    assert!(num_players > 0);
+
+    // create a GGRS session
+    let mut sess_build = SessionBuilder::<GGRSConfig>::new()
+        .with_num_players(num_players)
+        .with_max_prediction_window(12) // (optional) set max prediction window
+        .with_input_delay(2); // (optional) set input delay for the local player
+
+    // add players
+    for (i, player_addr) in opt.players.iter().enumerate() {
+        // local player
+        if player_addr == "localhost" {
+            sess_build = sess_build.add_player(PlayerType::Local, i)?;
+        } else {
+            // remote players
+            let remote_addr: SocketAddr = player_addr.parse()?;
+            sess_build = sess_build.add_player(PlayerType::Remote(remote_addr), i)?;
+        }
+    }
+
+    // optionally, add spectators
+    // for (i, spec_addr) in opt.spectators.iter().enumerate() {
+    //     sess_build = sess_build.add_player(PlayerType::Spectator(*spec_addr), num_players + i)?;
+    // }
+
+    // start the GGRS session
+    let socket = UdpNonBlockingSocket::bind_to_port(opt.local_port)?;
+    let sess = sess_build.start_p2p_session(socket)?;
+
+    let mut app = App::new();
+    GGRSPlugin::<GGRSConfig>::new()
+        // define frequency of rollback game logic update
+        .with_update_frequency(FPS)
+        // define system that returns inputs given a player handle, so GGRS can send the inputs around
+        .with_input_system(input)
+        // register types of components AND resources you want to be rolled back
+        .register_rollback_type::<Transform>()
+        .register_rollback_type::<Velocity>()
+        .register_rollback_type::<FrameCount>()
+        // these systems will be executed as part of the advance frame update
+        .with_rollback_schedule(
+            Schedule::default().with_stage(
+                ROLLBACK_DEFAULT,
+                SystemStage::parallel()
+                    .with_system(move_cube_system)
+                    .with_system(increase_frame_system),
+            ),
+        )
+        // make it happen in the bevy app
+        .build(&mut app);
+
+    // continue building/running the app like you normally would
+   // app.insert_resource(Msaa { samples: 4 })
+    app.insert_resource(WindowDescriptor {
+            width: 720.,
+            height: 720.,
+            title: "GGRS Box Game".to_owned(),
+            ..Default::default()
+        })
+        .insert_resource(opt)
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup_system)
+        // add your GGRS session
+        .insert_resource(sess)
+        .insert_resource(SessionType::P2PSession)
+        // register a resource that will be rolled back
+        .insert_resource(FrameCount { frame: 0 })
+        //print some network stats - not part of the rollback schedule as it does not need to be rolled back
+        .insert_resource(NetworkStatsTimer(Timer::from_seconds(2.0, true)))
+        .add_system(print_network_stats_system)
+        .add_system(print_events_system)
+        .run();
+
+    Ok(())
+}
+
+fn print_events_system(mut session: ResMut<P2PSession<GGRSConfig>>) {
+    for event in session.events() {
+        println!("GGRS Event: {:?}", event);
+    }
+}
+
+fn print_network_stats_system(
+    time: Res<Time>,
+    mut timer: ResMut<NetworkStatsTimer>,
+    p2p_session: Option<Res<P2PSession<GGRSConfig>>>,
+) {
+    // print only when timer runs out
+    if timer.0.tick(time.delta()).just_finished() {
+        if let Some(sess) = p2p_session {
+            let num_players = sess.num_players() as usize;
+            for i in 0..num_players {
+                if let Ok(stats) = sess.network_stats(i) {
+                    println!("NetworkStats for player {}: {:?}", i, stats);
+                }
+            }
+        }
+    }
+}
